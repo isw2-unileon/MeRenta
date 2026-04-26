d7fb9bcedbac55a7ff6e1dee051b9b3357290b8f
jmestd00 <jmestd00@estudiantes.unileon.es>
2026-04-26 19:12:54 +0200

    feat: add page layouts and update global styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = document.getElementById("root");
 
@@ -11,6 +12,8 @@ if (!root) {
 
 createRoot(root).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
